26051cef127bc6a0551be721f2426d771edd47d4
Alexander <alex1995599105@gmail.com>
2026-01-28 18:04:58 +0100

    publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,18 +1,28 @@
 import os
+import random
 from dotenv import load_dotenv
 from telegram import Bot
+
+
+NASA_DIR = 'NASA/apod'
 
 
 def send_message(token):
     bot = Bot(token=token)
 
     chat_id = -1003873120271
-    return bot.send_message(chat_id=chat_id, text="test")
+    images = os.listdir(NASA_DIR)
+    image_name = random.choice(images)
+    image_path = os.path.join(NASA_DIR, image_name)
+    with open(image_path, 'rb') as photo:
+        bot.send_photo(
+            chat_id=chat_id,
+            photo=photo,
+        )
 
 
 def main():
     load_dotenv()
-
     token = os.getenv('TOKEN')
     if not token:
         raise RuntimeError('TOKEN не найден в .env')
